0f2f8ab189cdb68df4845f4af2c88c95d6b7fa72
Jack <116763735+GitJackL@users.noreply.github.com>
2023-01-25 13:21:46 +0000

    added function to save input to local storage and to call back that input after reloading

--- starter/script.js
+++ starter/script.js
@@ -21,3 +21,69 @@ function updateColors() {
 
 setInitialColors();
 window.setInterval(updateColors, 1000);
+
+function saveText(hour) {
+  var text = $("#plan-col-" + hour).html();
+  localStorage.setItem("colorTime" + hour, text);
+}
+
+function displayText(hour) {
+  var text = localStorage.getItem("colorTime" + hour);
+  $("#plan-col-" + hour).html(text);
+}
+
+
+
+function addSaveListener(hour) {
+  $("#save-col-" + hour).click(function() {
+    saveText(hour);
+  });
+}
+
+$(document).ready(function() {
+  for (var i = 9; i <= 17; i++) {
+    displayText(i);
+  }
+});
+
+
+
+
+
+
+function saveText(hour) {
+  var text = $("#plan-col-" + hour).html();
+  localStorage.setItem("colorTime" + hour, text);
+}
+
+function displayText(hour) {
+  var text = localStorage.getItem("colorTime" + hour);
+  $("#plan-col-" + hour).html(text);
+}
+
+
+// This function adds a click event listener to the save button with the id "save-col-{hour}"
+function addSaveListener(hour) {
+  $("#save-col-" + hour).click(function() {
+    saveText(hour);
+  });
+}
+
+
+$(document).ready(function() {
+  for (var i = 9; i <= 17; i++) {
+    displayText(i);
+  }
+});
+
+
+// Here, you can call the addSaveListener function for each hour that you want to add a click event listener to
+addSaveListener(9);
+addSaveListener(10);
+addSaveListener(11);
+addSaveListener(12);
+addSaveListener(13);
+addSaveListener(14);
+addSaveListener(15);
+addSaveListener(16);
+addSaveListener(17);
