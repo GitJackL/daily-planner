12d5751073a790a24bbea76b6d0f630dc8d9c0fe
Jack <116763735+GitJackL@users.noreply.github.com>
2023-01-26 13:02:01 +0000

    cleaned up javescript code and added comments

--- starter/script.js
+++ starter/script.js
@@ -1,80 +1,60 @@
+// This function sets the initial background color of all the time blocks to white
 function setInitialColors() {
   for (var i = 9; i <= 17; i++) {
-    $("#colorTime" + (i)).css({"backgroundColor": "aliceblue"});
+    $("#colorTime" + (i)).css({ "backgroundColor": "white" });
   }
 }
-
+// This function updates the background color of the time blocks based on the current hour
 function updateColors() {
+  // Get the current hour
   var currentHour = moment().hours();
-  
+  // Loop through all the hours from 9 to 17
   for (var i = 9; i <= 17; i++) {
+    // If the current hour is equal to the hour we're looping through
+    // Set the background color of the corresponding time block to green
     if (i === currentHour) {
-      $("#colorTime" + (i)).css({"backgroundColor": "green"});
-    } else if(i < currentHour) {
-      $("#colorTime" + (i)).css({"backgroundColor": "red"});
+      $("#colorTime" + (i)).css({ "backgroundColor": "green" });
+      // If the current hour is less than the hour we're looping through
+      // Set the background color of the corresponding time block to red
+    } else if (i < currentHour) {
+      $("#colorTime" + (i)).css({ "backgroundColor": "red" });
     }
-    else{
-        $("#colorTime" + (i)).css({"backgroundColor": "aliceblue"});
+    // If the current hour is greater than the hour we're looping through
+    // Set the background color of the corresponding time block to aliceblue
+    else {
+      $("#colorTime" + (i)).css({ "backgroundColor": "aliceblue" });
     }
   }
 }
-
+// Call the setInitialColors function to set the initial background color of all the time blocks
 setInitialColors();
+// Call the updateColors function every 1000 milliseconds to update the background color
 window.setInterval(updateColors, 1000);
-
+// This function saves the text inside the text area with the id "plan-col-{hour}" to local storage
 function saveText(hour) {
   var text = $("#plan-col-" + hour).html();
   localStorage.setItem("colorTime" + hour, text);
 }
-
+// This function displays the text saved in local storage for a specific hour
 function displayText(hour) {
   var text = localStorage.getItem("colorTime" + hour);
   $("#plan-col-" + hour).html(text);
 }
 
-
-
-function addSaveListener(hour) {
-  $("#save-col-" + hour).click(function() {
-    saveText(hour);
-  });
-}
-
-$(document).ready(function() {
+// When the page has fully loaded, loops through  and calls the displayText function to each iteration.
+// Displays the rtext in the correspnding text area
+$(document).ready(function () {
   for (var i = 9; i <= 17; i++) {
     displayText(i);
   }
 });
-
-
-
-
-
-
-function saveText(hour) {
-  var text = $("#plan-col-" + hour).html();
-  localStorage.setItem("colorTime" + hour, text);
-}
-
-function displayText(hour) {
-  var text = localStorage.getItem("colorTime" + hour);
-  $("#plan-col-" + hour).html(text);
-}
-
 
 // This function adds a click event listener to the save button with the id "save-col-{hour}"
 function addSaveListener(hour) {
-  $("#save-col-" + hour).click(function() {
+  $("#save-col-" + hour).click(function () {
     saveText(hour);
   });
 }
-
-
-$(document).ready(function() {
-  for (var i = 9; i <= 17; i++) {
-    displayText(i);
-  }
-});
 
 
 // Here, you can call the addSaveListener function for each hour that you want to add a click event listener to
